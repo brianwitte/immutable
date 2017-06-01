fe7eaea7440a693d5dbce5858fe7e10deb54e0c9
walter <bwiyy@users.noreply.github.com>
2017-05-31 23:44:46 -0500

    removed some funcs

--- src/Main.purs
+++ src/Main.purs
@@ -4,16 +4,6 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
-combiner :: String -> String
-combiner x = x <> ", what?"
-
-iterate f 0 x = x
-iterate f n x = iterate f (n - 1) (f x)
-
-iAmANumber :: Number
-iAmANumber =
-  let square x = x * x
-  in square 42.0
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
